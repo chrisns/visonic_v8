918d0c9c1049c4bd4c311584989a4d95d89bbd6b
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-01-27 03:00:00 +0000

    example

--- index.ts
+++ index.ts
@@ -1,3 +1,21 @@
-export function helloworld () {
-  return
+import * as alarm from './src/alarm'
+const { hostname, app_id, user_code, panel_id, email, password } = process.env
+console.log(user_code)
+const run = async () => {
+  const authenticatedAxios = await alarm.getAuthenticatedAxios({
+    hostname,
+    app_id,
+    user_code,
+    user_token: null,
+    panel_id,
+    email,
+    password
+  })
+  const status = await alarm.getStatus(authenticatedAxios)
+  console.log(status)
+}
+try {
+  run()
+} catch (error) {
+  console.error(error)
 }
